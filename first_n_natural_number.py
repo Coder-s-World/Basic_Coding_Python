096b2c7c24676a6c5eeac9903ce12cfe9dc2da3a
Coder-s-World <mayuri.tukadiya@gmail.com>
2024-12-16 11:05:59 +0530

    Add files via upload

---
+++ first_n_natural_number.py
@@ -0,0 +1,19 @@
+# method 1
+# using for loop
+
+num = int(input("enter a number for 'for' loop: ")) # accept a number from user upto which number wants to print
+for i in range(1,num+1): # use for loop in which specify 'range' which is use to retun a sequence of number
+    print(i) 
+    
+# -------------------------------------------------------------------------------------------------
+
+# method 1
+# while loop
+
+num = int(input("enter a number for 'while' loop : ")) # accept a number from user upto which number wants to print
+count = 1
+while count <= num:
+    print(count)
+    count = count + 1
+    
+
