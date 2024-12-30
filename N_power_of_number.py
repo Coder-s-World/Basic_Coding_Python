80f89ce3b98eff830078eaabe147dbd628d442f1
Coder-s-World <mayuri.tukadiya@gmail.com>
2024-12-30 15:40:15 +0530

    Add files via upload

---
+++ N_power_of_number.py
@@ -0,0 +1,12 @@
+# method 1 using by default information
+base = int(input ("enter the base =  "))
+expo = int(input("enter the exponent = "))
+
+# pow is default function used to calculate the power of number
+power = pow(base,expo)
+print(f"the power of number is {power} ")
+
+
+# method 2  by using exponent operator
+results=5**5
+print(results)
